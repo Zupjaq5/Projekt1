02a569d9c7b9fcca78a43a9cc41a24875d5710b5
Zupjaq5 <82230511+Zupjaq5@users.noreply.github.com>
2022-09-04 11:40:58 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjektFinalVesionV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ProjektFinalVesionV2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Kontakt()
+        {
+            return View();
+        }
+
+        public IActionResult Galeria()
+        {
+            return View();
+        }
+
+        public IActionResult Partnerzy()
+        {
+            return View();
+        }
+
+        [HttpPost("/jsonString")]
+        public string SendForm(string jsonObj)
+        {
+            return " napis " + jsonObj.ToString();
+        }
+        public static void Email(string htmlString)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress("FromMailAddress");
+                message.To.Add(new MailAddress("ToMailAddress"));
+                message.Subject = "Test";
+                message.IsBodyHtml = true; //to make message body as html  
+                message.Body = htmlString;
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com"; //for gmail host  
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("FromMailAddress", "password");
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+            }
+            catch (Exception) { }
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,42 @@
+﻿function SendForm()
+{
+    var imie = document.querySelector("input[name=name]").value;
+    var nazwisko = document.querySelector("input[name=lastName]").value;
+    var email = document.querySelector("input[name=mailOfClient]").value;
+    var rodzajKlienta = document.querySelectorAll("input[name=TypeOfClient]");
+
+    var rodzajKlientaSelected = "";
+    if (rodzajKlienta[0].checked) {
+        rodzajKlientaSelected = "Osoba prywanta";
+    }
+    else if (rodzajKlienta[1].checked) {
+        rodzajKlientaSelected = "Firma";
+    }
+    else {
+        rodzajKlientaSelected = "Nie wybrano typu klienta!";
+    }
+
+    var komentarz = document.querySelector("textarea").value;
+
+    const json = '{"name":"' + imie + '", "surname":"' + nazwisko + '", "email":"' + email + '", "typeOfClient":"' + rodzajKlientaSelected + '", "content":"' + komentarz +'"}';
+    const obj = JSON.parse(json);
+    // metoda z neta ;>
+    $.ajax(
+        {
+            type: 'POST',
+            contentType: 'application/json',
+            url: '/Home/SendForm/' + json.toString(),
+            data: {
+                jsonObj: obj
+            },
+            success:
+                function (response) {
+                    console.log(response);
+                },
+            error:
+                function (response) {
+                    alert("Error: " + response);
+                }
+        });
+
+}
